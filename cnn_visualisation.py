3df2eaf74d8f2299ca05e3e98cab5bf89dafc249
utkuozbulak <utku.ozbulak@gmail.com>
2017-10-24 18:30:12 +0900

    Grad-cam update

--- cnn_visualisation.py
+++ cnn_visualisation.py
@@ -3,218 +3,307 @@ from torch.autograd import Variable
 from torch.autograd import Function
 from torchvision import models
 from torchvision import utils
-import cv2
+import cv2  # Only needed for GradCam heatmap
 import numpy as np
 from collections import OrderedDict
 from torch.nn import ReLU
 from PIL import Image
+from scipy.misc import imresize
+
 
 class CamExtractor():
+    """
+        Extracts cam features from the model
+    """
     def __init__(self, model, target_layer):
         self.model = model
         self.target_layer = target_layer
-        print(self.target_layer)
         self.gradients = None
 
     def save_gradient(self, grad):
+        print('Hooked on the feeling!')
         self.gradients = grad
 
     def forward_pass_on_convolutions(self, x):
+        """
+            Does a forward pass on convolutions, hooks the function at given layer
+        """
         conv_output = None
         for module_pos, module in self.model.features._modules.items():
-            x = module(x)
+            x = module(x)  # Forward
             if int(module_pos) == self.target_layer:
                 x.register_hook(self.save_gradient)
-                conv_output = x
+                conv_output = x  # Save the convolution output on that layer
         return conv_output, x
 
     def forward_pass(self, x):
+        """
+            Does a full forward pass on the model
+        """
+        # Forward pass on the convolutions
         conv_output, x = self.forward_pass_on_convolutions(x)
         x = x.view(x.size(0), -1)  # Flatten
+        # Forward pass on the classifier
         x = self.model.classifier(x)
         return conv_output, x
 
 
-class GradCam_v2:
+class GradCam():
+    """
+        Produces class activation map
+    """
     def __init__(self, model, target_layer):
-        print(target_layer)
         self.model = model
         self.model.eval()
+        # Define extractor
         self.extractor = CamExtractor(self.model, target_layer)
 
     def generate_cam(self, input_image, target_index=None):
-        # Forward pass
-        conv_outputs, model_output = self.extractor.forward_pass(input_image)
+        # Full forward pass
+        # conv_output is the output of convolutions at specified layer
+        # model_output is the final output of the model (1, 1000)
+        conv_output, model_output = self.extractor.forward_pass(input_image)
         if target_index is None:
             target_index = np.argmax(model_output.data.numpy())
-        # Target for guided backprop
+        # Target for backprop
         one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
         one_hot_output[0][target_index] = 1
         # Zero grads
         self.model.features.zero_grad()
         self.model.classifier.zero_grad()
-        # Backward pass
+        # Backward pass with specified target
         model_output.backward(gradient=one_hot_output, retain_graph=True)
-        # Get gradients
+        # Get hooked gradients
         guided_gradients = self.extractor.gradients.data.numpy()[0]
         # Get convolution outputs
-        target = conv_outputs.data.numpy()[0]
+        target = conv_output.data.numpy()[0]
         # Get weights from gradients
-        weights = np.mean(guided_gradients, axis=(1, 2))  # take averages for each gradient
+        weights = np.mean(guided_gradients, axis=(1, 2))  # Take averages for each gradient
         # Create empty numpy array for cam
         cam = np.ones(target.shape[1:], dtype=np.float32)
+        # Multiply each weight with its conv output and then, sum
         for i, w in enumerate(weights):
             cam += w * target[i, :, :]
         cam = np.maximum(cam, 0)
-        cam = cv2.resize(cam, (224, 224))
-        # Normalize between 0 - 1
-        cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))
+        cam = imresize(cam, (224, 224))
+        cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))  # Normalize between 0-1
+        cam = np.uint8(cam * 255)  # Scale between 0-255 to visualize
         return cam
 
-def PIL_process_image(PIL_img):
+
+def preprocess_image(PIL_img):
+    """
+        Processes image for CNNs
+
+    Args:
+        PIL_img (PIL_img): Image to process
+
+    returns:
+        im_as_var (Pytorch variable): Variable that contains processed float tensor
+
+    """
+    # mean and std list for channels (Imagenet)
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
-
+    # Resize image
     PIL_img = PIL_img.resize((224, 224), Image.ANTIALIAS)
+    # Convert to np array
     im_as_arr = np.array(PIL_img, dtype=np.float)
+    # Transpose to obtain D-W-H
     im_as_arr = im_as_arr.transpose(2, 0, 1)
+    # Normalize the channels
     for channel, _ in enumerate(im_as_arr):
         im_as_arr[channel] /= 255
         im_as_arr[channel] -= mean[channel]
         im_as_arr[channel] /= std[channel]
+    # Convert to float tensor
     im_as_ten = torch.from_numpy(im_as_arr).float()
+    # Add one more channel to the beginning. Tensor shape = 1,3,224,224
     im_as_ten.unsqueeze_(0)
+    # Convert to Pytorch variable
     im_as_var = Variable(im_as_ten, requires_grad=True)
     return im_as_var
 
 
-def preprocess_image(img):
-    means = [0.485, 0.456, 0.406]
-    stds = [0.229, 0.224, 0.225]
+def save_class_activation_on_image(org_img, activation_map, file_name):
+    """
+        Saves cam activation map and activation map on the original image
 
-    preprocessed_img = img.copy()[:, :, ::-1]
-    for i in range(3):
-        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] - means[i]
-        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] / stds[i]
-    preprocessed_img = \
-        np.ascontiguousarray(np.transpose(preprocessed_img, (2, 0, 1)))
-    preprocessed_img = torch.from_numpy(preprocessed_img)
-    preprocessed_img.unsqueeze_(0)
-    input = Variable(preprocessed_img, requires_grad=True)
-    return input
+    Args:
+        org_img (PIL img): Original image
+        activation_map (numpy arr): activation map (grayscale) 0-255
+        file_name (str): File name of the exported image
+    """
+    cv2.imwrite(file_name+'_Cam_Grayscale.jpg', activation_map)
+    activation_heatmap = cv2.applyColorMap(activation_map, cv2.COLORMAP_HSV)
+    cv2.imwrite(file_name+'_Cam_Heatmap.jpg', activation_heatmap)
 
-
-def show_cam_on_image(img, mask):
-    cv2.imwrite("vanilla-1.jpg", np.uint8(255 * mask))
-    heatmap = cv2.applyColorMap(np.uint8(255*mask), cv2.COLORMAP_BONE)
-    heatmap = np.float32(heatmap) / 255
-    cv2.imwrite("vanilla.jpg", np.uint8(255 * heatmap))
-    cam = heatmap + np.float32(img)
-    cam = cam / np.max(cam)
-    cv2.imwrite("cam.jpg", np.uint8(255 * cam))
+    img_with_heatmap = np.float32(activation_heatmap) + np.float32(org_img)
+    img_with_heatmap = img_with_heatmap / np.max(img_with_heatmap)
+    cv2.imwrite(file_name+'_Cam_OnImage.jpg', np.uint8(255 * img_with_heatmap))
 
 
-class Vanilla_Backprop():
-
-    def __init__(self, model, processed_im, layer, target_index):
+class VanillaBackprop():
+    """
+        Produces gradients generated with vanilla back propagation of the image
+    """
+    def __init__(self, model, processed_im, target_class):
         self.model = model
         self.input_image = processed_im
-        self.target_index = target_index
-        self.selected_laye = layer
-        self.gradients = []
-        self.all_grads = None
-        self.hook_gradients()
+        self.target_class = target_class
+        self.gradients = None
+        # Put model in evaluation mode
+        self.model.eval()
+        # Hook the first layer to get the gradient
+        self.hook_layers()
 
-
-    def hook_gradients(self):
+    def hook_layers(self):
         def hook_function(module, grad_in, grad_out):
-            print('Hooked on the feeling')
-            self.all_grads = grad_in[0]
+            print('Hooked on the feeling!')
+            self.gradients = grad_in[0]
 
-        list(pretrained_model.features._modules.items())[0][1].register_backward_hook(hook_function)
+        # Register hook to the first layer
+        first_layer = list(pretrained_model.features._modules.items())[0][1]
+        first_layer.register_backward_hook(hook_function)
 
     def run(self):
-
-        model_output = self.model(self.input_image)  # Forward
+        # Forward
+        model_output = self.model(self.input_image)
+        # Zero grads
         self.model.zero_grad()
+        # Target for backprop
         one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
-        one_hot_output[0][self.target_index] = 1
+        one_hot_output[0][self.target_class] = 1
+        # Backward pass
         model_output.backward(gradient=one_hot_output)
-        return self.all_grads
+        # Convert Pytorch variable to numpy array
+        # [0] to get rid of the first channel (1,3,224,224)
+        gradients_as_arr = self.gradients.data.numpy()[0]
+        return gradients_as_arr
 
 
-class GBP():
-
-    def __init__(self, model, processed_im, layer, target_index):
+class GuidedBackprop():
+    """
+       Produces gradients generated with guided back propagation of the given image
+    """
+    def __init__(self, model, processed_im, target_index):
         self.model = model
         self.input_image = processed_im
         self.target_index = target_index
-        self.selected_laye = layer
-        self.gradients = []
-        self.all_grads = None
-        self.updated_relus()
-        self.hook_gradients()
+        self.gradients = None
+        # Put model in evaluation mode
+        self.model.eval()
+        self.update_relus()
+        self.hook_layers()
 
-
-    def hook_gradients(self):
+    def hook_layers(self):
         def hook_function(module, grad_in, grad_out):
-            print('Hooked on the feeling')
-            self.all_grads = grad_in[0]
+            print('Hooked on the feeling!')
+            self.gradients = grad_in[0]
 
-        list(pretrained_model.features._modules.items())[0][1].register_backward_hook(hook_function)
+        # Register hook to the first layer
+        first_layer = list(pretrained_model.features._modules.items())[0][1]
+        first_layer.register_backward_hook(hook_function)
 
-    def updated_relus(self):
+    def update_relus(self):
+        """
+            Updates relu activation functions so that it only returns positive gradients
+        """
         def relu_hook_function(module, grad_in, grad_out):
+            """
+            If there is a negative gradient, changes it to zero
+            """
             if isinstance(module, ReLU):
-                print('Hooked on the relu')
                 return (torch.clamp(grad_in[0], min=0.0),)
-
+        # Loop through layers, hook up ReLUs with relu_hook_function
         for pos, module in pretrained_model.features._modules.items():
             if isinstance(module, ReLU):
                 module.register_backward_hook(relu_hook_function)
 
-
-
     def run(self):
-
-        model_output = self.model(self.input_image)  # Forward
+        # Forward pass
+        model_output = self.model(self.input_image)
+        # Zero gradients
         self.model.zero_grad()
+        # Target for backprop
         one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
         one_hot_output[0][self.target_index] = 1
+        # Backward pass
         model_output.backward(gradient=one_hot_output)
-        return self.all_grads
+        # Convert Pytorch variable to numpy array
+        # [0] to get rid of the first channel (1,3,224,224)
+        gradients_as_arr = self.gradients.data.numpy()[0]
+        return gradients_as_arr
 
+
+def save_grad_black_background(gradient, file_name):
+    """
+        Exports the gradient image on top of black background
+
+    Args:
+        gradient (np arr): Numpy array of the gradient with shape (3, 224, 224)
+        file_name (str): File name to be exported
+    """
+    black_grad = np.maximum(gradient, 0)
+    black_grad /= np.max(black_grad)
+    black_grad = np.uint8(black_grad * 255).transpose(1, 2, 0)
+    cv2.imwrite(file_name+'_Black_.jpg', black_grad)
+
+
+def save_grad_original(gradient, file_name):
+    """
+        Exports the original gradient image
+
+    Args:
+        gradient (np arr): Numpy array of the gradient with shape (3, 224, 224)
+        file_name (str): File name to be exported
+    """
+    org_grad = gradient - gradient.min()
+    org_grad /= org_grad.max()
+    org_grad = np.uint8(org_grad * 255).transpose(1,2,0)
+    cv2.imwrite(file_name+'_Org.jpg', org_grad)
+
+
+def guided_grad_cam(guided_prop, grad_cam):
+    pass
 
 
 
 if __name__ == '__main__':
-    """ python grad_cam.py <path_to_image>
-    1. Loads an image with opencv.
-    2. Preprocesses it for VGG19 and converts to a pytorch variable.
-    3. Makes a forward pass to find the category index with the highest score,
-    and computes intermediate activations.
-    Makes the visualization.
-    """
-    # Can work with any model, but it assumes that the model has a
-    # feature method, and a classifier method,
-    # as in the VGG models in torchvision.
-    img = cv2.imread('examples/both.png')
-    img = np.float32(cv2.resize(img, (224, 224))) / 255
-    prep_im = preprocess_image(img)
 
     imv2 = Image.open('examples/both.png')
-    prep_imv2 = PIL_process_image(imv2)
+    prep_im = preprocess_image(imv2)
 
     pretrained_model=models.vgg19(pretrained=True)
 
     """
-    gcv2 = GradCam_v2(pretrained_model, target_layer = 35)
+    gcv2 = GradCam(pretrained_model, target_layer = 35)
     m = gcv2.generate_cam(prep_im, 282)
-    show_cam_on_image(img, m)
+    save_class_activation_on_image(imv2,m, 'cat_boy')
     """
 
-    z = GBP(pretrained_model, prep_im, 0, 999)
-    all_grads = z.run()
+    VBP = VanillaBackprop(pretrained_model, prep_im, 282)
+    all_grads = VBP.run()
+    save_grad_black_background(all_grads, 'Vanilla_BP')
+    save_grad_original(all_grads, 'Vanilla_BP')
+    utils.save_image(torch.from_numpy(all_grads), 'van.jpg')
+
+    VBP = GuidedBackprop(pretrained_model, prep_im, 282)
+    all_grads = VBP.run()
+    save_grad_black_background(all_grads, 'Guided_BP')
+    save_grad_original(all_grads, 'Guided_BP')
+    utils.save_image(torch.from_numpy(all_grads), 'gbp.jpg')
+
+
+
+
+    """
+    z = GBP(pretrained_model, prep_im, 999)
+    all_grads = z.run()ü
+
+    """
+    """
+
     all_grads = all_grads.data.numpy()[0]
     all_grads -= all_grads.min()
     all_grads /= all_grads.max()
@@ -225,7 +314,7 @@ if __name__ == '__main__':
     show_cam_on_image(img, all_grads)
     # If None, returns the map for the highest scoring category.
     # Otherwise, targets the requested index.
-
+    """
 
     """
     gb_model = GuidedBackpropReLUModel(model=models.vgg19(pretrained=True),
