80c0ab70a190838f36c460b2673aa343d166bc3a
utkuozbulak <utku.ozbulak@gmail.com>
2017-11-26 23:49:50 +0900

    Included layer vis. without hooks

--- src/cnn_layer_visualization.py
+++ src/cnn_layer_visualization.py
@@ -27,8 +27,6 @@ class CNNLayerVisualization():
         self.conv_output = 0
         # Generate a random image
         self.created_image = np.uint8(np.random.uniform(150, 180, (224, 224, 3)))
-        # Hook the layers to get result of the convolution
-        self.hook_layer()
         # Create the folder to export images if not exists
         if not os.path.exists('generated'):
             os.makedirs('generated')
@@ -41,22 +39,66 @@ class CNNLayerVisualization():
         # Hook the selected layer
         self.model[self.selected_layer].register_forward_hook(hook_function)
 
-    def visualise_layer(self):
+    def visualise_layer_with_hooks(self):
+        # Hook the selected layer
+        self.hook_layer()
         # Process image and return variable
         self.processed_image = preprocess_image(self.created_image)
         # Define optimizer for the image
-        # Earlier layers need higher learning rates to visualize whereas layer layers need less
+        # Earlier layers need higher learning rates to visualize whereas later layers need less
         optimizer = SGD([self.processed_image], lr=5, weight_decay=1e-6)
         for i in range(1, 51):
             optimizer.zero_grad()
             # Assign create image to a variable to move forward in the model
             x = self.processed_image
             for index, layer in enumerate(self.model):
-                # Forward
+                # Forward pass layer by layer
+                # x is not used after this point because it is only needed to trigger
+                # the forward hook function
                 x = layer(x)
-                # Only need to forward until we the selected layer is reached
+                # Only need to forward until the selected layer is reached
                 if index == self.selected_layer:
+                    # (forward hook function triggered)
                     break
+            # Loss function is the mean of the output of the selected layer/filter
+            # We try to minimize the mean of the output of that specific filter
+            loss = torch.mean(self.conv_output)
+            print('Iteration:', str(i), 'Loss:', "{0:.2f}".format(loss.data.numpy()[0]))
+            # Backward
+            loss.backward()
+            # Update image
+            optimizer.step()
+            # Recreate image
+            self.created_image = recreate_image(self.processed_image)
+            # Save image
+            if i % 5 == 0:
+                cv2.imwrite('generated/layer_vis_l' + str(self.selected_layer) +
+                            '_f' + str(self.selected_filter) + '_iter'+str(i)+'.jpg',
+                            self.created_image)
+
+    def visualise_layer_without_hooks(self):
+        # Process image and return variable
+        self.processed_image = preprocess_image(self.created_image)
+        # Define optimizer for the image
+        # Earlier layers need higher learning rates to visualize whereas later layers need less
+        optimizer = SGD([self.processed_image], lr=5, weight_decay=1e-6)
+        for i in range(1, 51):
+            optimizer.zero_grad()
+            # Assign create image to a variable to move forward in the model
+            x = self.processed_image
+            for index, layer in enumerate(self.model):
+                # Forward pass layer by layer
+                x = layer(x)
+                if index == self.selected_layer:
+                    # Only need to forward until the selected layer is reached
+                    # Now, x is the output of the selected layer
+                    break
+            # Here, we get the specific filter from the output of the convolution operation
+            # x is a tensor of shape 1x512x28x28.(For layer 17)
+            # So there are 512 unique filter outputs
+            # Following line selects a filter from 512 filters so self.conv_output will become
+            # a tensor of shape 28x28
+            self.conv_output = x[0, self.selected_filter]
             # Loss function is the mean of the output of the selected layer/filter
             # We try to minimize the mean of the output of that specific filter
             loss = torch.mean(self.conv_output)
@@ -80,4 +122,9 @@ if __name__ == '__main__':
     # Fully connected layer is not needed
     pretrained_model = models.vgg16(pretrained=True).features
     layer_vis = CNNLayerVisualization(pretrained_model, cnn_layer, filter_pos)
-    layer_vis.visualise_layer()
+
+    # Layer visualization with pytorch hooks
+    # layer_vis.visualise_layer_with_hooks()
+
+    # Layer visualization without pytorch hooks
+    layer_vis.visualise_layer_without_hooks()
