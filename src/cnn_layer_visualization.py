993f040e0f9f8d3fcc38e1817c9fdfb18588d189
utkuozbulak <utku.ozbulak@gmail.com>
2018-06-07 14:50:21 +0900

    Update layer vis

--- src/cnn_layer_visualization.py
+++ src/cnn_layer_visualization.py
@@ -8,7 +8,7 @@ import cv2
 import numpy as np
 
 import torch
-from torch.optim import SGD
+from torch.optim import Adam
 from torchvision import models
 
 from misc_functions import preprocess_image, recreate_image
@@ -45,9 +45,8 @@ class CNNLayerVisualization():
         # Process image and return variable
         self.processed_image = preprocess_image(self.created_image)
         # Define optimizer for the image
-        # Earlier layers need higher learning rates to visualize whereas later layers need less
-        optimizer = SGD([self.processed_image], lr=5, weight_decay=1e-6)
-        for i in range(1, 51):
+        optimizer = Adam([self.processed_image], lr=0.1, weight_decay=1e-6)
+        for i in range(1, 31):
             optimizer.zero_grad()
             # Assign create image to a variable to move forward in the model
             x = self.processed_image
@@ -80,9 +79,8 @@ class CNNLayerVisualization():
         # Process image and return variable
         self.processed_image = preprocess_image(self.created_image)
         # Define optimizer for the image
-        # Earlier layers need higher learning rates to visualize whereas later layers need less
-        optimizer = SGD([self.processed_image], lr=6, weight_decay=1e-6)
-        for i in range(1, 51):
+        optimizer = Adam([self.processed_image], lr=0.1, weight_decay=1e-6)
+        for i in range(1, 31):
             optimizer.zero_grad()
             # Assign create image to a variable to move forward in the model
             x = self.processed_image
@@ -118,13 +116,13 @@ class CNNLayerVisualization():
 
 if __name__ == '__main__':
     cnn_layer = 17
-    filter_pos = 15
+    filter_pos = 5
     # Fully connected layer is not needed
     pretrained_model = models.vgg16(pretrained=True).features
     layer_vis = CNNLayerVisualization(pretrained_model, cnn_layer, filter_pos)
 
     # Layer visualization with pytorch hooks
-    # layer_vis.visualise_layer_with_hooks()
+    layer_vis.visualise_layer_with_hooks()
 
     # Layer visualization without pytorch hooks
-    layer_vis.visualise_layer_without_hooks()
+    # layer_vis.visualise_layer_without_hooks()
