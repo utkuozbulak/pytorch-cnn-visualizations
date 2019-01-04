5d052f1043362e83b4934d56343f05c28894a807
utkuozbulak <utku.ozbulak@gmail.com>
2019-01-04 16:53:32 +0900

    Updated guided backprop

--- src/guided_backprop.py
+++ src/guided_backprop.py
@@ -19,6 +19,7 @@ class GuidedBackprop():
     def __init__(self, model):
         self.model = model
         self.gradients = None
+        self.forward_relu_outputs = []
         # Put model in evaluation mode
         self.model.eval()
         self.update_relus()
@@ -27,25 +28,37 @@ class GuidedBackprop():
     def hook_layers(self):
         def hook_function(module, grad_in, grad_out):
             self.gradients = grad_in[0]
-
         # Register hook to the first layer
         first_layer = list(self.model.features._modules.items())[0][1]
         first_layer.register_backward_hook(hook_function)
 
     def update_relus(self):
         """
-            Updates relu activation functions so that it only returns positive gradients
+            Updates relu activation functions so that
+                1- stores output in forward pass
+                2- imputes zero for gradient values that are less than zero
         """
-        def relu_hook_function(module, grad_in, grad_out):
+        def relu_backward_hook_function(module, grad_in, grad_out):
             """
-            If there is a negative gradient, changes it to zero
+            If there is a negative gradient, change it to zero
             """
-            if isinstance(module, ReLU):
-                return (torch.clamp(grad_in[0], min=0.0),)
-        # Loop through layers, hook up ReLUs with relu_hook_function
+            # Get last forward output
+            corresponding_forward_output = self.forward_relu_outputs[-1]
+            corresponding_forward_output[corresponding_forward_output > 0] = 1
+            modified_grad_out = corresponding_forward_output * torch.clamp(grad_in[0], min=0.0)
+            del self.forward_relu_outputs[-1]  # Remove last forward output
+            return (modified_grad_out,)
+
+        def relu_forward_hook_function(module, ten_in, ten_out):
+            """
+            Store results of forward pass
+            """
+            self.forward_relu_outputs.append(ten_out)
+        # Loop through layers, hook up ReLUs
         for pos, module in self.model.features._modules.items():
             if isinstance(module, ReLU):
-                module.register_backward_hook(relu_hook_function)
+                module.register_backward_hook(relu_backward_hook_function)
+                module.register_forward_hook(relu_forward_hook_function)
 
     def generate_gradients(self, input_image, target_class):
         # Forward pass
