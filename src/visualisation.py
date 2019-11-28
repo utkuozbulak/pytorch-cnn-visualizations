b69b5fbf0a31ef490c627b2ce71ebe48c9c187df
ea409 <33399273+ea409@users.noreply.github.com>
2019-11-28 15:20:13 +0000

    Create visualisation.py
    This just merges together vanilla backprop, guided backprop and the guided layer specific backprop. Also deep copy instead of shallow copy on the model for each visualization because then it's possible to load multiple visualization types at the same time. It also automatically enables gradients on the input images.

---
+++ src/visualisation.py
@@ -0,0 +1,114 @@
+import torch
+from torch.nn import ReLU
+import copy
+
+class Visualisation(object):
+    """Base class for all Visualisations.
+    Every Sampler subclass has to provide a generate gradients method. 
+    """
+    def __init__(self, model):
+        self.model = copy.deepcopy(model)
+        self.gradients = None 
+
+    def hook_layers(self): 
+        def hook_function(module, grad_in, grad_out):
+            self.gradients = grad_in[0]
+        first_layer = list(self.model.features._modules.items())[0][1]
+        first_layer.register_backward_hook(hook_function)  
+
+    def generate_gradients(self, input_image, target_class):
+        raise NotImplementedError
+
+class Vanilla(Visualisation): 
+    def __init__(self, *args, **kwargs):
+        super(Vanilla, self).__init__(*args, **kwargs)
+        self.model.eval()
+        self.hook_layers()
+        
+    def generate_gradients(self, input_image, target_class):
+        input_image.requires_grad = True
+        # Forward
+        model_output = self.model(input_image)
+        # Zero grads
+        self.model.zero_grad()
+        # Target for backprop
+        one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
+        one_hot_output[0][target_class] = 1
+        # Backward pass
+        model_output.backward(gradient=one_hot_output)
+        # Convert Pytorch variable to numpy array
+        # [0] to get rid of the first channel (1,3,224,224)
+        gradients_as_arr = self.gradients.data.numpy()[0]
+        return gradients_as_arr
+    
+class Guided(Visualisation):
+    def __init__(self, *args, **kwargs):
+        super(Guided, self).__init__(*args, **kwargs)
+        self.forward_relu_outputs = []
+        self.model.eval()
+        self.update_relus()
+        self.hook_layers()
+
+    def update_relus(self):
+        """
+            Updates relu activation functions so that
+                1- stores output in forward pass
+                2- imputes zero for gradient values that are less than zero
+        """
+        def relu_backward_hook_function(module, grad_in, grad_out):
+            """
+            If there is a negative gradient, change it to zero
+            """
+            # Get last forward output
+            corresponding_forward_output = self.forward_relu_outputs[-1]
+            corresponding_forward_output[corresponding_forward_output > 0] = 1
+            modified_grad_out = corresponding_forward_output * torch.clamp(grad_in[0], min=0.0)
+            del self.forward_relu_outputs[-1]  # Remove last forward output
+            return (modified_grad_out,)
+
+        def relu_forward_hook_function(module, ten_in, ten_out):
+            """
+            Store results of forward pass
+            """
+            self.forward_relu_outputs.append(ten_out)
+        # Loop through layers, hook up ReLUs
+        for pos, module in self.model.features._modules.items():
+            if isinstance(module, ReLU):
+                module.register_backward_hook(relu_backward_hook_function)
+                module.register_forward_hook(relu_forward_hook_function) 
+
+    def generate_gradients(self, input_image, target_class):
+        input_image.requires_grad = True
+        # Forward pass
+        model_output = self.model(input_image)
+        # Zero gradients
+        self.model.zero_grad()
+        # Target for backprop
+        one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
+        one_hot_output[0][target_class] = 1
+        # Backward pass
+        model_output.backward(gradient=one_hot_output)
+        # Convert Pytorch variable to numpy array
+        # [0] to get rid of the first channel (1,3,224,224)
+        gradients_as_arr = self.gradients.data.numpy()[0]
+        return gradients_as_arr
+
+class GuidedLayerSpecific(Guided):
+    def generate_gradients(self, input_image, target_class, cnn_layer, filter_pos):
+        self.model.zero_grad()
+        # Forward pass
+        x = input_image
+        for index, layer in enumerate(self.model.features):
+            # Forward pass layer by layer
+            # x is not used after this point because it is only needed to trigger
+            # the forward hook function
+            x = layer(x)
+            # Only need to forward until the selected layer is reached
+            if index == cnn_layer:
+                # (forward hook function triggered)
+                break
+        conv_output = torch.sum(torch.abs(x[0, filter_pos]))
+        # Backward pass
+        conv_output.backward()
+        gradients_as_arr = self.gradients.data.numpy()[0]
+        return gradients_as_arr   
